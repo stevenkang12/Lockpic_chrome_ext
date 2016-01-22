10960e29a16dce3af30f03b00f9161b3ce3cb191
Steven Kang <stevenkang@Stevens-MBP-2.wireless.yale.internal>
2016-01-22 15:02:15 -0500

    created the popup window

---
+++ content.js
@@ -0,0 +1,12 @@
+// content.js
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      // var firstHref = $("a[href^='http']").eq(0).attr("href");
+      // console.log(firstHref);
+
+      // sends message to background.js to open a new tab with the url
+      // chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
+    }
+  }
+);
